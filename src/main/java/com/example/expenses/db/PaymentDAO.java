050f26d6e48b1edb8c89a78676127ab706509801
nfernis <af7935053@gmail.com>
2026-04-09 23:09:46 +0300

    make listenernterxpenses 50/50, make insertayment

--- src/main/java/com/example/expenses/db/PaymentDAO.java
+++ src/main/java/com/example/expenses/db/PaymentDAO.java
@@ -11,7 +11,26 @@ import java.util.logging.Logger;
 public class PaymentDAO {
     private static final Logger LOGGER = Logger.getLogger(BalanceDAO.class.getName());
 
-    /**/
+    /*Метод для записи платежей в БД*/
+    public static void insertPayment(float amount, String type, boolean is_paid) throws SQLException {
+        String query ="insert into payments(type, amount, is_paid)\n" +
+                "values (?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection()){
+            PreparedStatement ps = conn.prepareStatement(query);
+            LOGGER.info( "Подлючились к supabase из insertPayment()");
+            ps.setString(1, type);        // 1-й знак вопроса -> type
+            ps.setFloat(2, amount);       // 2-й знак вопроса -> amount
+            ps.setBoolean(3, is_paid);    // 3-й знак вопроса -> is_paid
+            ps.executeUpdate();
+        }catch (Exception e) {
+            LOGGER.severe("Не удалось подключиться к supabase из insertPayment()");
+            throw new SQLException(e);
+        }
+
+
+
+    }
+
 
 
 }
--- src/main/java/com/example/expenses/service/EnterExpenses.java
+++ src/main/java/com/example/expenses/service/EnterExpenses.java
@@ -1,10 +1,14 @@
+/*Класс, предназначенный для внесения планируемых или уже состоявшихся расходов с правой панели*/
 package com.example.expenses.service;
 
 import com.example.expenses.db.BalanceDAO;
 import com.example.expenses.utils.CustomLevel;
 
 import javax.swing.*;
+import java.sql.SQLException;
 import java.util.logging.Logger;
+
+import static com.example.expenses.db.PaymentDAO.insertPayment;
 
 public class EnterExpenses {
     private static final Logger LOGGER = Logger.getLogger(BalanceDAO.class.getName());
@@ -13,8 +17,14 @@ public class EnterExpenses {
         enterButton.addActionListener(e -> {
             for(JTextField i : fields){
                 if(!i.getText().isEmpty() && i.getText().matches("\\d+(\\.\\d+)?")){
+
                     /*метод для записи в БД*/
-                    LOGGER.log(CustomLevel.TEST, "Йоу, мы ща все в БД запишем: " + i.getText());
+                    try {
+                        insertPayment(100, "Аренда", true);
+                        LOGGER.log(CustomLevel.TEST, "Йоу, мы ща все в БД запишем: " + i.getText());
+                    }catch(SQLException ex) {
+                        throw new RuntimeException(ex);
+                    }
 
                 }
             }
